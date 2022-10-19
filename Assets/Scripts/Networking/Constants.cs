959aa5b2f988b19cf1e4f75c76c82996c64d0afc
Nick H <nickh@bitheads.com>
2022-10-19 18:31:01 -0400

    Upgraded brainCloud Unity plugin

---
+++ Assets/Scripts/Networking/Constants.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Constants
+{
+    public static string LobbyStrategy = "strategy";
+    public static string StrategyCompound = "compound";
+    public static string StrategyAbsolute = "absolute";
+    public static string LobbyAlignment = "alignment";
+    public static string UserItemsServiceCriteria = "criteria";
+    public static string CriteraPing = "ping";
+    public static string StrategyRangedPercent = "ranged-percent";
+    public static string AlignmentCenter = "center";
+    public static string CriteraRating = "rating";
+    public static string CompoundAlgos = "algos";
+    public static string CompoundRanges = "compound-ranges";
+}
